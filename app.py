0bb4d5bd92445bcd63ac74833cd7d43331412cb8
kushal <70305822+kushalsubedi@users.noreply.github.com>
2024-05-23 09:32:24 +0545

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_hit_count():
 @app.route('/')
 def hello_route():
     count = get_hit_count()
+    print("Good it works")
     return f"Hello World From Docker !! I have seen this {count}times"
 
 
